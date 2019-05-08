0b13be4e9352c6571a36b03e72ea1166dc7acd06
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-08 19:06:41 -0400

    class App extends Component
    - constructor and set state

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,25 +2,13 @@ import REact, { Component } from 'react';
 import axios from "axios";
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: []
+    };
+  }
 }
 
 export default App;
