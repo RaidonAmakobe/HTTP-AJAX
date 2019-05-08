388bddd46bebbe9a92314c77cf3e1b8ccbda1e97
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-08 19:19:57 -0400

    render()
    map through data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,5 @@
-import REact, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-
 
 class App extends Component {
   constructor() {
@@ -9,14 +8,22 @@ class App extends Component {
       friendsList: []
     };
   }
-}
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friendsList: res.data }))
+      .catch(err => console.log(err));
+  }
 
-componentDidMount() {
-  axios
-  .get("http://localhost:5000/friends")
-  .then(res => this.setState({ friendsList: res.data})
-  .catch(err => console.log(err));
-  )
+  render() {
+    return (
+      <div className="App">
+        {this.state.friendsList.map(pal => (
+          <p key={pal.id}>{pal.name}</p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
